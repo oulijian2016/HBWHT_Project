9a8f1635a4fdc34d72885261ce72d831f119e0d5
oulijian <15201315450@163.com>
2016-05-19 14:54:25 +0800

    湖北文化厅20160519提交

--- com.jiuqi.hbswht.member/src/com/jiuqi/hbswht/member/constant/WhtConstant.java
+++ com.jiuqi.hbswht.member/src/com/jiuqi/hbswht/member/constant/WhtConstant.java
@@ -18,4 +18,50 @@ public class WhtConstant {
 	 * 湖北文化厅申报表
 	 */
 	public static String F_HBWHTSBB_ZTYJ = "ZTYJ";// 总体意见
+	
+	/**
+	 * 湖北文化厅目标明细数量指标子表
+	 */
+	public static String T_SLZB = "HBWHT_MBMX_SLZB";
+	
+	/**
+	 * 湖北文化厅目标明细质量指标子表
+	 */
+	public static String T_ZLZB = "HBWHT_MBMX_ZLZB";
+	
+	/**
+	 * 湖北文化厅目标明细时效指标子表
+	 */
+	public static String T_SXZB = "HBWHT_MBMX_SXZB";
+	
+	/**
+	 * 湖北文化厅目标明细成本指标子表
+	 */
+	public static String T_CBZB = "HBWHT_MBMX_CBZB";
+	
+	/**
+	 * 湖北文化厅目标明细经济效益指标子表
+	 */
+	public static String T_JJXYZB = "HBWHT_MBMX_JJXYZB";
+	
+	/**
+	 * 湖北文化厅目标明细社会效益指标子表
+	 */
+	public static String T_SHXYZB = "HBWHT_MBMX_SHXYZB";
+	
+	/**
+	 * 湖北文化厅目标明细生态效益指标子表
+	 */
+	public static String T_STXYZB = "HBWHT_MBMX_STXYZB";
+	
+	/**
+	 * 湖北文化厅目标明细可持续影响指标子表
+	 */
+	public static String T_KCXYXZB = "HBWHT_MBMX_KCXYXZB";
+	
+	/**
+	 * 湖北文化厅目标明细服务对象满意度指标子表
+	 */
+	public static String T_FWDXMYDZB = "HBWHT_MBMX_FWDXMYDZB";
+	
 }
